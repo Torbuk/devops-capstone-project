e4215eea3830fc052e3c1d01038de22a37f2ec3f
Torbuk <robert.cacija@gmail.com>
2024-06-27 05:00:18 -0400

    Update accounts functions

--- service/routes.py
+++ service/routes.py
@@ -101,7 +101,7 @@ def get_accounts(account_id):
 def update_accounts(account_id):
     """
     Update an Account
-    This endpoint will update an Account based on the posted data
+    This endpoint will update an Account based on the posted data.
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
